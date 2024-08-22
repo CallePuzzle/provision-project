14788a01572fee31d88ff0b15604fa0313a51482
César M. Cristóbal <cesar@callepuzzle.com>
2024-08-22 08:34:57 +0200

    desactivamos mongodb

--- 000-module/providers.tf
+++ 000-module/providers.tf
@@ -8,10 +8,6 @@ terraform {
       source  = "cloudflare/cloudflare"
       version = "4.35.0"
     }
-    mongodbatlas = {
-      source  = "mongodb/mongodbatlas"
-      version = "1.17.4"
-    }
     auth0 = {
       source  = "auth0/auth0"
       version = "1.4.0"
