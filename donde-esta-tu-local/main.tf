60ee94f93fd6e51d29dbd44ab50d3d8278deb333
César M. Cristóbal <cesar@callepuzzle.com>
2024-08-30 09:08:49 +0200

    usamos sops para las credeciales de los providers

--- donde-esta-tu-local/main.tf
+++ donde-esta-tu-local/main.tf
@@ -13,12 +13,43 @@ terraform {
       source  = "carlpett/sops"
       version = "1.1.1"
     }
+    github = {
+      source  = "integrations/github"
+      version = "6.2.2"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "4.35.0"
+    }
+    auth0 = {
+      source  = "auth0/auth0"
+      version = "1.4.0"
+    }
   }
 }
 
 data "sops_file" "extra_secrets" {
   source_file = "extra-secrets.enc.json"
   input_type  = "json"
+}
+
+data "sops_file" "credentials" {
+  source_file = "credentials.enc.json"
+  input_type  = "json"
+}
+
+provider "github" {
+  token = data.sops_file.credentials.data["GITHUB_TOKEN"]
+  owner = data.sops_file.credentials.data["GITHUB_OWNER"]
+}
+
+provider "cloudflare" {
+  api_token = data.sops_file.credentials.data["CLOUDFLARE_API_TOKEN"]
+}
+
+provider "auth0" {
+  domain = data.sops_file.credentials.data["AUTH0_DOMAIN"]
+  # token from AUTH0_API_TOKEN in .env file
 }
 
 module "this" {
